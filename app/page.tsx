7e9f57253e6f1b86f62218f399ceb0bd76a3d16e
Dimitri Almon <dimitri.almon@gmail.com>
2025-01-21 11:25:05 +0100

    🧑‍🦽 BASED FINALLY

--- app/page.tsx
+++ app/page.tsx
@@ -4,72 +4,72 @@ import * as Constants from '@common/constants';
 import * as Utilities from '@common/utilities';
 
 import Accordion from '@components/Accordion';
-import ActionBar from '@components/ActionBar';
+// import ActionBar from '@components/ActionBar';
 import ActionButton from '@components/ActionButton';
-import ActionListItem from '@components/ActionListItem';
-import AlertBanner from '@components/AlertBanner';
-import AS400 from '@components/examples/AS400';
-import Avatar from '@components/Avatar';
-import Badge from '@components/Badge';
-import BarLoader from '@components/BarLoader';
-import BarProgress from '@components/BarProgress';
-import Block from '@components/Block';
-import BlockLoader from '@components/BlockLoader';
-import Breadcrumbs from '@components/BreadCrumbs';
-import Button from '@components/Button';
-import ButtonGroup from '@components/ButtonGroup';
+// import ActionListItem from '@components/ActionListItem';
+// import AlertBanner from '@components/AlertBanner';
+// import AS400 from '@components/examples/AS400';
+// import Avatar from '@components/Avatar';
+// import Badge from '@components/Badge';
+// import BarLoader from '@components/BarLoader';
+// import BarProgress from '@components/BarProgress';
+// import Block from '@components/Block';
+// import BlockLoader from '@components/BlockLoader';
+// import Breadcrumbs from '@components/BreadCrumbs';
+// import Button from '@components/Button';
+// import ButtonGroup from '@components/ButtonGroup';
 import Card from '@components/Card';
-import CardDouble from '@components/CardDouble';
-import Checkbox from '@components/Checkbox';
-import Chessboard from '@components/Chessboard';
-import CodeBlock from '@components/CodeBlock';
-import ContentFluid from '@components/ContentFluid';
-import ComboBox from '@components/ComboBox';
-import DataTable from '@components/DataTable';
-import DatePicker from '@components/DatePicker';
-import DashboardRadar from '@components/examples/DashboardRadar';
+// import CardDouble from '@components/CardDouble';
+// import Checkbox from '@components/Checkbox';
+// import Chessboard from '@components/Chessboard';
+// import CodeBlock from '@components/CodeBlock';
+// import ContentFluid from '@components/ContentFluid';
+// import ComboBox from '@components/ComboBox';
+// import DataTable from '@components/DataTable';
+// import DatePicker from '@components/DatePicker';
+// import DashboardRadar from '@components/examples/DashboardRadar';
 import DebugGrid from '@components/DebugGrid';
 import DefaultActionBar from '@components/page/DefaultActionBar';
 import DefaultLayout from '@components/page/DefaultLayout';
-import Denabase from '@components/examples/Denabase';
-import Dialog from '@components/Dialog';
-import Divider from '@components/Divider';
-import Drawer from '@components/Drawer';
-import DropdownMenuTrigger from '@components/DropdownMenuTrigger';
+// import Denabase from '@components/examples/Denabase';
+// import Dialog from '@components/Dialog';
+// import Divider from '@components/Divider';
+// import Drawer from '@components/Drawer';
+// import DropdownMenuTrigger from '@components/DropdownMenuTrigger';
 import Grid from '@components/Grid';
-import HoverComponentTrigger from '@components/HoverComponentTrigger';
-import Indent from '@components/Indent';
-import Input from '@components/Input';
-import IntDevLogo from '@components/svg/IntDevLogo';
-import ListItem from '@components/ListItem';
+// import HoverComponentTrigger from '@components/HoverComponentTrigger';
+// import Indent from '@components/Indent';
+// import Input from '@components/Input';
+// import IntDevLogo from '@components/svg/IntDevLogo';
+// import ListItem from '@components/ListItem';
 import Message from '@components/Message';
 import MessageViewer from '@components/MessageViewer';
 import MessagesInterface from '@components/examples/MessagesInterface';
-import ModalAlert from '@components/modals/ModalAlert';
-import ModalCanvasSnake from '@components/modals/ModalCanvasSnake';
-import ModalCanvasPlatformer from '@components/modals/ModalCanvasPlatformer';
-import ModalChess from '@components/modals/ModalChess';
+// import ModalAlert from '@components/modals/ModalAlert';
+// import ModalCanvasSnake from '@components/modals/ModalCanvasSnake';
+// import ModalCanvasPlatformer from '@components/modals/ModalCanvasPlatformer';
+// import ModalChess from '@components/modals/ModalChess';
 import ModalCreateAccount from '@components/modals/ModalCreateAccount';
 import ModalError from '@components/modals/ModalError';
-import ModalMatrixModes from '@components/modals/ModalMatrixModes';
+// import ModalMatrixModes from '@components/modals/ModalMatrixModes';
 import ModalStack from '@components/ModalStack';
 import ModalTrigger from '@components/ModalTrigger';
 import Navigation from '@components/Navigation';
-import NumberRangeSlider from '@components/NumberRangeSlider';
+// import NumberRangeSlider from '@components/NumberRangeSlider';
 import Package from '@root/package.json';
-import RadioButtonGroup from '@components/RadioButtonGroup';
-import Row from '@components/Row';
-import RowSpaceBetween from '@components/RowSpaceBetween';
-import Script from 'next/script';
-import Select from '@components/Select';
-import SidebarLayout from '@components/SidebarLayout';
-import Table from '@components/Table';
-import TableRow from '@components/TableRow';
-import TableColumn from '@components/TableColumn';
-import Text from '@components/Text';
-import TextArea from '@components/TextArea';
+// import RadioButtonGroup from '@components/RadioButtonGroup';
+// import Row from '@components/Row';
+// import RowSpaceBetween from '@components/RowSpaceBetween';
+// import Script from 'next/script';
+// import Select from '@components/Select';
+// import SidebarLayout from '@components/SidebarLayout';
+// import Table from '@components/Table';
+// import TableRow from '@components/TableRow';
+// import TableColumn from '@components/TableColumn';
+// import Text from '@components/Text';
+// import TextArea from '@components/TextArea';
 import TreeView from '@components/TreeView';
-import UpdatingDataTable from '@components/examples/UpdatingDataTable';
+// import UpdatingDataTable from '@components/examples/UpdatingDataTable';
 
 export const dynamic = 'force-static';
 
@@ -177,12 +177,12 @@ export default async function Page(props) {
           <br />
         </Accordion>
 
-        <Accordion defaultValue={true} title="DASHBOARD RADAR">
+        {/* <Accordion defaultValue={true} title="DASHBOARD RADAR">
           <Card>
               <DashboardRadar />
           </Card>
           <br />
-        </Accordion>
+        </Accordion> */}
 
         <Accordion defaultValue={true} title="MESSAGES INTERFACE">
           This example combines the aesthetics of iMessage and IRSSI with a terminal-inspired design. It shows how easy it is to embed a simple web application into your website.
